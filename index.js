c942cf5fe667e99021f57a33a8f237a89d4e00fc
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-09-14 17:51:14 -0300

    project basis

--- index.js
+++ index.js
@@ -5,6 +5,35 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+let DB ={
+    games:[
+        {
+            id: 0,
+            title: "Call of Duty",
+            year: 2019,
+            price: 60
+
+        },
+        {
+            id: 1,
+            title: "Assassin's Creed IV",
+            year: 2013,
+            price: 90
+
+        },
+        {
+            id: 2,
+            title: "Assassin's Creed Valhalla",
+            year: 2020,
+            price: 120
+
+        }
+    ]
+}
+
+app.get("/",()=>{
+
+});
 
 app.listen(8080,()=>{
     console.log("API RODANDO!");
